838506e38d1f40894b9c5ce7f73caeb6693d36d0
Jesse_Bogaart <jesseb99@live.nl>
2024-01-29 12:18:13 +0100

    word counter update

--- word_counter.js
+++ word_counter.js
@@ -1,22 +1,12 @@
-function numberOfWords(str) {
-    let count = 0;
-    let check = false;
+const inputField = document.getElementById('inputField');
+const wordCountSpan = document.getElementById('wordCount');
 
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== ' ' && !check) {
-            count++;
-            check = true;
-        } else if (str[i] === ' ') {
-            check = false;
-        }
-    }
-
-    return count;
+function updateWordCount() {
+    const inputText = inputField.value.trim();
+    const wordCount = inputText.split(/\s+/).filter(word => word.length > 0).length;
+    wordCountSpan.textContent = wordCount;
 }
+inputField.addEventListener('input', updateWordCount);
 
+updateWordCount();
 
-console.log("Word count:", numberOfWords(str));
-
-
-backgorund 300 
-motivatie 150
